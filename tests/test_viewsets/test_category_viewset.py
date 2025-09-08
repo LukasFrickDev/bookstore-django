70cd4be166c14e9d20a908e68a8df6db0dc93006
Lukas Frick <contatolukasfrick@gmail.com>
2025-09-08 15:07:23 -0300

    django-authenticator

--- tests/test_viewsets/test_category_viewset.py
+++ tests/test_viewsets/test_category_viewset.py
@@ -2,27 +2,36 @@ import json
 
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
+from rest_framework.authtoken.models import Token
 
 from django.urls import reverse
 
 from product.factories import CategoryFactory
 from product.models import Category
+from order.factories import UserFactory
 
 class TestCategoryViewSet(APITestCase):
     
     client = APIClient()
     
     def setUp(self):
+        self.user = UserFactory()
         self.category = CategoryFactory(title='books')
+        token = Token.objects.create(user=self.user)
+        token.save()
 
     def test_get_all_category(self):
+        token = Token.objects.get(user__username=self.user.username)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)  
         response = self.client.get(reverse('category-list', kwargs={'version': 'v1'}))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        category_data = json.loads(response.content)[0]
-        self.assertEqual(category_data['title'], self.category.title)       
+        category_data = json.loads(response.content)
+        self.assertEqual(category_data['results'][0]['title'], self.category.title)
 
     def test_create_category(self):
+        token = Token.objects.get(user__username=self.user.username)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)  
         data = json.dumps({
             "title": 'technology'
         })
