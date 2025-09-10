93291a526d4c0984974a66e223db1703d18b45a3
Lukas Frick <contatolukasfrick@gmail.com>
2025-09-10 16:18:20 -0300

    Configuração pythonanywhere

--- product/models/product.py
+++ product/models/product.py
@@ -7,3 +7,6 @@ class Product(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=10, null=True)
     active = models.BooleanField(default=True)
     categories = models.ManyToManyField("Category", blank=True)
+
+    def __str__(self):
+        return self.title
